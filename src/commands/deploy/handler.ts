8eee8a2a024eefbf8b2346b6325b31a3833a1819
Rob Moran <github@thegecko.org>
2018-02-21 15:29:09 +0000

    Fixed image deletion when building on device (#9)
    * Reintroduce dockerode types since fixes
    
    * Fixed image deletion when building on device

--- src/commands/deploy/handler.ts
+++ src/commands/deploy/handler.ts
@@ -31,7 +31,10 @@ export class Handler {
         this.docker = new Docker(this.args.address);
     }
 
-    private getStream(): Promise<NodeJS.ReadableStream> {
+    /**
+     * Build and/or open the image to ensure it's available
+     */
+    private ensureImage(returnStream: boolean = true): Promise<NodeJS.ReadableStream> {
         if (!existsSync(this.args.source)) {
             return Promise.reject(`Unable to find source '${this.args.source}'`);
             // Assume source is a URL or registry entry
@@ -41,9 +44,7 @@ export class Handler {
         if (lstatSync(this.args.source).isDirectory()) {
             // Build directory and return resulting stream
             const builder = new Builder(this.args);
-            const streamToDevice = (this.args.address !== this.args.remote);
-
-            return builder.build(streamToDevice);
+            return builder.build(returnStream);
         } else {
             // Return stream from file
             log(`Found application '${this.args.source}'`);
@@ -52,39 +53,51 @@ export class Handler {
         }
     }
 
-    public run() {
+    /**
+     * Deploy the image stream deleting any existing image
+     */
+    private deployStream(stream: NodeJS.ReadableStream) {
         const deployer: Deployer = new DockerDeployer(this.docker);
         deployer.addListener(DockerDeployer.EVENT_LOG, log);
 
-        this.getStream()
+        return this.docker.inspectImage(true)
+        .then(imageInfo => {
+            if (imageInfo && imageInfo.Id) {
+                log("Removing old application...");
+                return this.docker.deleteImage(false, true, imageInfo.Id);
+            }
+        })
+        .then(() => {
+            if (stream) {
+                return deployer.deploy(stream);
+            }
+        });
+    }
+
+    public run() {
+        const buildingOffDevice = (this.args.address !== this.args.remote);
+
+        this.ensureImage(buildingOffDevice)
         .then(imageStream => {
 
-            return this.docker.inspectImage(true)
-            .then(imageInfo => {
-                log("Stopping any application...");
-                return this.docker.stopContainer(true)
-                .then(() => this.docker.deleteContainer(true))
-                .then(() => {
-                    if (imageInfo && imageInfo.Id) {
-                        return this.docker.deleteImage(false, true, imageInfo.Id);
-                    }
-                })
-                .then(() => {
-                    if (imageStream) {
-                        return deployer.deploy(imageStream);
-                    }
-                })
-                .then(() => {
-                    log("Starting application...");
-                    return this.docker.createContainer();
-                })
-                .then(() => this.docker.startContainer())
-                .then(() => {
-                    if (!this.args.detached) {
-                        return this.docker.attachContainer()
-                        .then(() => log("Attached to application"));
-                    }
-                });
+            log("Stopping any running application...");
+            return this.docker.stopContainer(true)
+            .then(() => this.docker.deleteContainer(true))
+            .then(() => {
+                if (buildingOffDevice) {
+                    return this.deployStream(imageStream);
+                }
+            })
+            .then(() => {
+                log("Starting application...");
+                return this.docker.createContainer();
+            })
+            .then(() => this.docker.startContainer())
+            .then(() => {
+                if (!this.args.detached) {
+                    return this.docker.attachContainer()
+                    .then(() => log("Attached to application"));
+                }
             });
         })
         .catch(error => log(`Error: ${error}`));
