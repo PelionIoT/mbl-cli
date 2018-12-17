c60592f4f602951b1979619999c45fd73709ff06
Robert Walton <rob.walton@arm.com>
2018-12-17 13:52:52 +0000

    SQUASH: build: don't run sh scripts with sudo

--- build-wheel.sh
+++ build-wheel.sh
@@ -3,8 +3,8 @@
 JOB_NAME="mbl-cli"
 BUILD_NUMBER="0.1.1"
 
-sudo docker build -t build -f Dockerfile .
-sudo docker run  --name $JOB_NAME-$BUILD_NUMBER-build build
-sudo docker cp $JOB_NAME-$BUILD_NUMBER-build:/mbl-cli/dist $PWD
-sudo docker rm $JOB_NAME-$BUILD_NUMBER-build
+docker build -t build -f Dockerfile .
+docker run  --name $JOB_NAME-$BUILD_NUMBER-build build
+docker cp $JOB_NAME-$BUILD_NUMBER-build:/mbl-cli/dist $PWD
+docker rm $JOB_NAME-$BUILD_NUMBER-build
 
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,8 +3,8 @@
 JOB_NAME="mbl-cli"
 BUILD_NUMBER="0.1.1"
 
-sudo docker build -t build -f ./Dockerfile ../
-sudo docker run  --name $JOB_NAME-$BUILD_NUMBER-test build
-sudo docker cp $JOB_NAME-$BUILD_NUMBER-test:/mbl-cli/report $PWD
-sudo docker rm $JOB_NAME-$BUILD_NUMBER-test
+docker build -t build -f ./Dockerfile ../
+docker run  --name $JOB_NAME-$BUILD_NUMBER-test build
+docker cp $JOB_NAME-$BUILD_NUMBER-test:/mbl-cli/report $PWD
+docker rm $JOB_NAME-$BUILD_NUMBER-test
 
