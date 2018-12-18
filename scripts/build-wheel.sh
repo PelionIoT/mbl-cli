1054847b578e0cd35c7059db264c0510b4fef45e
Robert Walton <rob.walton@arm.com>
2018-12-18 14:43:11 +0000

    SQUASH: move shell script to scripts directory

--- scripts/build-wheel.sh
+++ scripts/build-wheel.sh
@@ -5,6 +5,6 @@ BUILD_NUMBER="0.1.1"
 
 docker build -t build -f Dockerfile .
 docker run  --name $JOB_NAME-$BUILD_NUMBER-build build
-docker cp $JOB_NAME-$BUILD_NUMBER-build:/mbl-cli/dist $PWD
+docker cp $JOB_NAME-$BUILD_NUMBER-build:/mbl-cli/dist $PWD/../
 docker rm $JOB_NAME-$BUILD_NUMBER-build
 
