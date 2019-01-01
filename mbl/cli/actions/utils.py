88393e5e9fbcc31cf6bf921d8f65b2b120aa0d72
Robert Walton <rob.walton@arm.com>
2019-01-01 19:29:01 +0000

    SQUASH: clean up utils

--- mbl/cli/actions/utils.py
+++ mbl/cli/actions/utils.py
@@ -5,16 +5,8 @@
 
 """Action handler helper functions/classes."""
 
-import pathlib
-import re
 import socket
-import sys
-import threading
-import time
-
-from mbl.cli.utils import device, file_handler, ssh
-
-JSON_FILE_PATH = str(pathlib.Path().home() / ".mbl-dev.json")
+from mbl.cli.utils import device, file_handler
 
 
 def create_device(args):
@@ -26,17 +18,12 @@ def create_device(args):
         if is_valid_ipv4_address(args.address) or is_valid_ipv6_address(
             args.address
         ):
-            return device.create_device("", args.address)
+            data = {"hostname": "", "address": args.address}
         else:
-            return device.create_device(args.address, "")
+            data = {"hostname": args.address, "address": ""}
     else:
-        return _create_device_from_data(_read_device_file())
-
-
-def save_device_info(device, path=JSON_FILE_PATH):
-    """Save device info to a file."""
-    fh = file_handler.DeviceInfoFileHandler(path)
-    fh.save_device_data(device)
+        data = file_handler.read_device_file()
+    return device.create_device(**data)
 
 
 def is_valid_ipv4_address(address):
@@ -63,22 +50,3 @@ def is_valid_ipv6_address(address):
         return False
     else:
         return True
-
-
-def _read_device_file(path=JSON_FILE_PATH):
-    """Read the json file and return some data."""
-    fh = file_handler.DeviceInfoFileHandler(path)
-    try:
-        device_data = fh.read_device_data()
-    except FileNotFoundError as file_err:
-        raise IOError("Please select a device or provide an ip address.")
-    else:
-        return device_data
-
-
-def _create_device_from_data(data):
-    """Create a DeviceInfo object using the file data.
-
-    :param data: data as returned from _read_device_file.
-    """
-    return device.create_device(*data.values())
