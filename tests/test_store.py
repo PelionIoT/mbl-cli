e7d74f79a29d4916cadd61c65d5a6f7509e9c2af
Rob Walton <rob.walton@arm.com>
2019-01-29 09:28:18 +0000

    To be squashed: licensing

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
 """Tests for the Store classes."""
 
 import pathlib
