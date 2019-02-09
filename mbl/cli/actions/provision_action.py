4e95338faafdf758a7c04446b069e6dd7470c067
Robert Walton <rob.walton@arm.com>
2019-02-09 23:18:43 +0000

    begin to implement provision action

--- mbl/cli/actions/provision_action.py
+++ mbl/cli/actions/provision_action.py
@@ -1,5 +1,13 @@
 from mbl.cli.utils import certificates
+from mbl.cli.utils.store import Store
 
 
 def execute(args):
-    certificates.create_developer_cert("dev_two")
+    if args.create_cert:
+        key_name, cert_name = args.create_cert
+        store_handle = Store("user")
+        api_key = store_handle.api_keys[key_name]
+        cert = certificates.create_developer_cert(api_key, cert_name)
+        team_store_handle = Store("team")
+        team_store_handle.dev_certs[cert_name] = cert
+        team_store_handle.save()
