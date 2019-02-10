74d129530cda162ba7e81550a786bcfa6a3ccb56
Robert Walton <rob.walton@arm.com>
2019-02-10 00:19:06 +0000

    implement cert header creation and retrieval

--- mbl/cli/actions/provision_action.py
+++ mbl/cli/actions/provision_action.py
@@ -3,11 +3,17 @@ from mbl.cli.utils.store import Store
 
 
 def execute(args):
+    cert_name = args.cert_name
     if args.create_cert:
-        key_name, cert_name = args.create_cert
+        key_name = args.create_cert
         store_handle = Store("user")
         api_key = store_handle.api_keys[key_name]
-        cert = certificates.create_developer_cert(api_key, cert_name)
+        cert_header = certificates.create_developer_cert(api_key, cert_name)
+        parsed = certificates.parse_cert_header(cert_header)
         team_store_handle = Store("team")
-        team_store_handle.dev_certs[cert_name] = cert
+        team_store_handle.dev_certs[cert_name] = parsed
         team_store_handle.save()
+    else:
+        sh = Store("team")
+        cert_header = sh.dev_certs[cert_name]
+        print(cert_header)
