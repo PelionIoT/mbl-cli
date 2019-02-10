0230866fc950a72155434f184389432e1b6e0957
Robert Walton <rob.walton@arm.com>
2019-02-10 00:19:37 +0000

    tweak parser args

--- mbl/cli/args/parser.py
+++ mbl/cli/args/parser.py
@@ -101,14 +101,16 @@ def parse_args(description):
 
     provision = command_group.add_parser("provision-pelion")
     provision.add_argument(
+        "cert_name", help="Name of the certificate to fetch/create."
+    )
+    provision.add_argument(
         "-c",
         "--create-cert",
-        nargs=2,
-        metavar="<key-name> <cert-name>",
+        metavar="<key-name>",
         help="Create a new developer certificate and inject it into the"
         " selected device. "
         "<key-name>: the name of the API key saved using save-api-key. "
-        "<cert-name>: the name to give the developer cert being created."
+        "Used to access the Pelion API to create the cert."
     )
     provision.set_defaults(func=provision_action.execute)
 
