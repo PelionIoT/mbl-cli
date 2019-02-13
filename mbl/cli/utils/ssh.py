0d2ccf5d24f8f400e3d2c5d3b6acb8dbb330012c
Rob Walton <rob.walton@arm.com>
2019-02-13 16:04:30 +0000

    Fix issues with ipv6 address checking

--- mbl/cli/utils/ssh.py
+++ mbl/cli/utils/ssh.py
@@ -81,22 +81,11 @@ class SSHSession:
 
     def __enter__(self):
         """Enter the context, connect to the ssh session."""
-        if self.device.hostname:
-            qualified_hostname = "{}.local".format(self.device.hostname)
-        else:
-            qualified_hostname = self.device.address
-        try:
-            self._client.connect(
-                qualified_hostname,
-                username=self.device.username,
-                password=self.device.password,
-            )
-        except socket.gaierror:
-            self._client.connect(
-                self.device.address,
-                username=self.device.username,
-                password=self.device.password,
-            )
+        self._client.connect(
+            self.device.address,
+            username=self.device.username,
+            password=self.device.password,
+        )
         return self
 
     def __exit__(self, *exception_info):
