1afeb09d2043f284cb1d29571a46f033091a87b6
Rob Walton <rob.walton@arm.com>
2019-03-05 20:22:50 +0000

    Improve shell command output and return codes (#23)
    * Improve shell command output and return codes

--- mbl/cli/mbl_cli.py
+++ mbl/cli/mbl_cli.py
@@ -8,10 +8,18 @@
 A toolbox for managing target devices running Mbed Linux OS.
 """
 
+import enum
 import sys
 import traceback
 
 from mbl.cli.args import parser
+
+
+class ExitCode(enum.Enum):
+    """Application return codes."""
+
+    SUCCESS = 0
+    ERROR = 256
 
 
 def _main():
@@ -19,10 +27,14 @@ def _main():
         args = parser.parse_args(description=__doc__)
         args.func(args)
     except Exception as error:
+        if hasattr(error, "return_code"):
+            ret_code = error.return_code
+        else:
+            ret_code = ExitCode.ERROR.value
         if args.verbose:
             traceback.print_exc()
         else:
             print(error, file=sys.stderr)
-        return 1
+        return ret_code
     else:
-        return 0
+        return ExitCode.SUCCESS.value
