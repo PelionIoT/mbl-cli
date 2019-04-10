2c42cefdaceafe1b6aebae22b72c2451e5d4ab22
Rob Walton <rob.walton@arm.com>
2019-04-10 14:15:28 +0100

    add more error checking and match ip exactly

--- tests/integration/test_device_connectivity_after_reboot.py
+++ tests/integration/test_device_connectivity_after_reboot.py
@@ -15,17 +15,19 @@ class TestListCommand:
             cli_cmd = "mbl-cli -a {} shell 'su -l -c reboot'".format(_address)
         else:
             cli_cmd = "mbl-cli shell 'su -l -c reboot'"
-        pexpect.run(cli_cmd)
-        time.sleep(20)
+        reboot_output, exit_status = pexpect.run(cli_cmd, withexitstatus=True)
+        assert exit_status == 0
+
+        time.sleep(40)
         for i in range(15):
             mbl_cli = pexpect.run("mbl-cli list")
             ipv4_match = re.search(
                 r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", mbl_cli.decode()
             )
-            name_match = re.search(
-                r"\d: mbed-linux-os-9164: ", mbl_cli.decode()
+            ip_match = re.search(
+                r"{}".format(_address), mbl_cli.decode()
             )
-            if name_match is not None:
+            if ip_match is not None:
                 break
-        assert name_match
+        assert ip_match
         assert not ipv4_match
