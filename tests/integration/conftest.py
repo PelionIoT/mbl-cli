9852605393bdabfc1ba6c8086c4d4dbd95311fa4
Rob Walton <rob.walton@arm.com>
2019-04-15 13:05:16 +0100

    Add LAVA tests for basic commands (#46)
    Add pytests to be executed through LAVA, to test the MBL CLI basic commands when interacting with a real device.

---
+++ tests/integration/conftest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Pytest configuration file."""
+
+
+def pytest_report_teststatus(report):
+    """Override Pytest hook to report test status."""
+    if report.when == "call":
+        if report.passed:
+            lava = "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={} RESULT=pass>".format(
+                report.nodeid.replace(" ", "_")
+            )
+            return report.outcome, "*", lava
+
+    if report.failed:
+        lava = "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={} RESULT=fail>".format(
+            report.nodeid.replace(" ", "_")
+        )
+        return report.outcome, "*", lava
